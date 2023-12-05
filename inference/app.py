f2b90947d8e31508045feb128df39ce638a9e296
rcarrata <rcarratalasanchez@gmail.com>
2023-12-05 12:13:26 +0100

    add falcon and inference app

--- inference/app.py
+++ inference/app.py
@@ -1,5 +1,6 @@
 from langchain.llms import HuggingFaceTextGenInference
 import argparse
+import requests
 
 # Set up argparse to accept a command-line argument for the client IP address
 parser = argparse.ArgumentParser(description="Test LLM inference server")
@@ -7,16 +8,38 @@ parser.add_argument("--url", type=str, required=True, help="URL of inference ser
 parser.add_argument("--input", type=str, required=False, help="Input text to generate from")
 args = parser.parse_args()
 
-# Create an instance of HuggingFaceTextGenInference
-llm = HuggingFaceTextGenInference(
-  inference_server_url=args.url if args.url else "http://hf-tgi-server.llms.svc.cluster.local:3000/",
-  max_new_tokens=512,
-  top_k=10,
-  top_p=0.95,
-  typical_p=0.95,
-  temperature=0.01,
-  repetition_penalty=1.03,
-)
+def get_info(url):
+  response = requests.get(f"{url}/info")
+  print(response.json())
+  return response.json()
+
+def get_generated_text(input_text):
+    # Create an instance of HuggingFaceTextGenInference
+    llm = HuggingFaceTextGenInference(
+        inference_server_url=args.url if args.url else "http://hf-tgi-server.llms.svc.cluster.local:3000/",
+        max_new_tokens=512,
+        top_k=10,
+        top_p=0.95,
+        typical_p=0.95,
+        temperature=0.01,
+        repetition_penalty=1.03,
+    )
+
+    # Generate text using the llm instance
+    generated_text = llm(input_text)
+    return generated_text
+
+
+# # Create an instance of HuggingFaceTextGenInference
+# llm = HuggingFaceTextGenInference(
+#   inference_server_url=args.url if args.url else "http://hf-tgi-server.llms.svc.cluster.local:3000/",
+#   max_new_tokens=512,
+#   top_k=10,
+#   top_p=0.95,
+#   typical_p=0.95,
+#   temperature=0.01,
+#   repetition_penalty=1.03,
+# )
 
 # Generate text using the llm instance
 if not args.input:
@@ -25,8 +48,11 @@ else:
   args.input = args.input
 
 input_text = args.input
-generated_text = llm(input_text)
+generated_text = get_generated_text(input_text)
 
 # Print the generated text
 print("Input text:", input_text)
 print(generated_text)
+
+print("Model Info:")
+get_info(args.url)
